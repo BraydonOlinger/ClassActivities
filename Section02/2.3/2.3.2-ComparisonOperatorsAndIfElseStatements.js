454cc700429a692d0750fc104a15ae22d119f93c
Braydon Olinger <braydonolingermktg@gmail.com>
2024-04-17 21:30:04 -0500

    2.3.2 Activity

--- Section02/2.3/2.3.2-ComparisonOperatorsAndIfElseStatements.js
+++ Section02/2.3/2.3.2-ComparisonOperatorsAndIfElseStatements.js
@@ -6,10 +6,10 @@ let age = 25;
 // console.log "Renting a car is more affordable."
 // if age is less than 25
 // console.log "Renting a car is very expensive."
-if(age >= 25) {
+if (age >= 25) {
   console.log("Renting a car is more affordable.");
 } else {
-  console.log("Renting a car is very expensive.")
+  console.log("Renting a car is very expensive.");
 }
 
 // Chaining if statements: if - else if - ... - else
@@ -20,22 +20,15 @@ if(age >= 25) {
 // console.log "Renting a car is very expensive."
 // if age is less than 18
 // console.log "You cannot legally rent a car."
-if(age >= 25) {
-  console.log("Renting a car is more affordable.")
-} else if(age < 25 && age >=18); {
-  console.log("Renting a car is very expensive");
-} else {
-  console.log("You cannot legally rent a car.");
-}
 
 if (age >= 25) {
-  consolelog ("Renting a car is more affordable.")
+  console.log("Renting a car is more affordable.");
 }
 
 if (age < 25 && age >= 18) {
-  console.log("Renting a car is very expensive")
+  console.log("Renting a car is very expensive");
 }
 
 if (age < 18) {
-  console.log(You cannot legally rent a car.);
+  console.log("You cannot legally rent a car.");
 }
