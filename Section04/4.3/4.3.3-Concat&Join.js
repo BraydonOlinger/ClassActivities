987ab28d71e11fb10b87a41ca1415f585c0fd4fb
Braydon Olinger <braydonolingermktg@gmail.com>
2024-05-01 21:21:54 -0500

    Palindrome Test

--- Section04/4.3/4.3.3-Concat&Join.js
+++ Section04/4.3/4.3.3-Concat&Join.js
@@ -7,8 +7,22 @@ console.log(arrThree);
 
 // print the new, merged array
 console.log(arrThree);
+console.log(arrThree.reverse());
 
 // join the merged array and print the result
 let newString = arrThree.join(", and the next value is ")
 
-console.log(typeof newString);
+// console.log(typeof newString);
+
+// Write a function that accepts one string as an argument and returns a statement
+// declaring whether it is or is not a palindrome
+function palindromeTest(word){
+    let revword = word.split('').reverse().join('');
+    if (word === revword){
+        console.log("Equal")
+    } else {
+        console.log("Nope")
+    }
+}
+
+let word = "RaceCar"
